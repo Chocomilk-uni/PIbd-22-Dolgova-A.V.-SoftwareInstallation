95d2d2eae0cb6f46658dca26f663749b01a3ed5d
Долгова А. В <70472233+Chocomilk-uni@users.noreply.github.com>
2021-03-08 18:27:33 +0400

    Revert "labwork1 (complexed) fix"
    This reverts commit 86932a8db897179ca2a655089d9e85ef27878e5d.

--- SoftwareInstallation/SoftwareInstallationBusinessLogic/ViewModels/WarehouseViewModel.cs
+++ SoftwareInstallation/SoftwareInstallationBusinessLogic/ViewModels/WarehouseViewModel.cs
@@ -6,7 +6,7 @@ namespace SoftwareInstallationBusinessLogic.ViewModels
 {
     public class WarehouseViewModel
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
 
         [DisplayName("Название склада")]
         public string WarehouseName { get; set; }
