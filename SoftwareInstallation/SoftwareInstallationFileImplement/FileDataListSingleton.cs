d55a730bf7b27f8971d6de2ea07e386b573381ea
Долгова А. В <kawa.no.kiri@gmail.com>
2021-05-09 10:54:35 +0400

    merge lab5(base) into lab6(base)

--- SoftwareInstallation/SoftwareInstallationFileImplement/FileDataListSingleton.cs
+++ SoftwareInstallation/SoftwareInstallationFileImplement/FileDataListSingleton.cs
@@ -142,7 +142,7 @@ namespace SoftwareInstallationFileImplement
                     list.Add(new Client
                     {
                         Id = Convert.ToInt32(elem.Attribute("Id").Value),
-                        FIO = elem.Element("FIO").Value,
+                        ClientFIO = elem.Element("FIO").Value,
                         Email = elem.Element("Email").Value,
                         Password = elem.Element("Password").Value
                     });
@@ -251,7 +251,7 @@ namespace SoftwareInstallationFileImplement
                 {
                     xElement.Add(new XElement("Client",
                         new XAttribute("Id", client.Id),
-                        new XElement("FIO", client.FIO),
+                        new XElement("FIO", client.ClientFIO),
                         new XElement("Email", client.Email),
                         new XElement("Password", client.Password)));
                 }
