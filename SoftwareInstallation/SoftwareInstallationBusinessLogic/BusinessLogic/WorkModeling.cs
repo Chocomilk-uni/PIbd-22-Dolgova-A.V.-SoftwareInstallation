be9350687dc77804c8d4a63a28f466e6b581302c
Долгова А. В <kawa.no.kiri@gmail.com>
2021-05-18 12:14:36 +0400

    mini fix

--- SoftwareInstallation/SoftwareInstallationBusinessLogic/BusinessLogic/WorkModeling.cs
+++ SoftwareInstallation/SoftwareInstallationBusinessLogic/BusinessLogic/WorkModeling.cs
@@ -4,7 +4,6 @@ using SoftwareInstallationBusinessLogic.Interfaces;
 using SoftwareInstallationBusinessLogic.ViewModels;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
